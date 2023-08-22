0b960364f42384cb88fae90021caf0ed5abe9a29
16114 <1611448204@qq.com>
2023-08-22 21:20:59 +0800

    新分支

---
+++ src/main/java/DevTest.java
@@ -0,0 +1,9 @@
+public class DevTest {
+    public static void main(String[] args) {
+        System.out.println("新分支");
+        System.out.println("新分支");
+        System.out.println("新分支");
+        System.out.println("新分支");
+        System.out.println("新分支");
+    }
+}
